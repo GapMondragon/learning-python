01fa1ac0f99c46a769f7d32e421f990bfa4ac89f
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-04 03:07:07 +0800

    Heads or Tails

---
+++ 4- Randomisation and Lists/headsORtails.py
@@ -0,0 +1,29 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+# simple heads or tails app
+
+import random
+
+# get random integer between 1 and 10
+random_side = random.randint(0,1)
+
+print('''TOSSING
+...
+ ...
+  ...
+ ...
+...
+      ''')
+if random_side == 0:
+    print("HEADS!")
+elif random_side == 1:
+    print("TAILS!!")
+
+
+
+
+
+
+
+
