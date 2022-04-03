afd0e2cb40d97f838d56b1e4dbdb8c110be91a83
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-04 02:39:15 +0800

    randomisation samples

---
+++ 4- Randomisation and Lists/samples.py
@@ -0,0 +1,17 @@
+import random
+
+# get random integer between 1 and 10
+random_integer = random.randint(1,10)
+print(random_integer)
+
+# this is how modules work:
+# import my_pi.py
+import my_pi
+print(my_pi.pi)
+
+# generate random from 0 - 0.99999
+random_float = random.random()
+print(random_float)
+# if you want to have more than 0.9999 multiply it 
+
+
