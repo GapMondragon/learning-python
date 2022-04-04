b6f66a8c3df3edbea91b6b6f8ff19c921f3fc82b
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-04 18:26:38 +0800

    List Samples - Appex, Extend, Get data from the last item

---
+++ 4- Randomisation and Lists/list-samples.py
@@ -0,0 +1,28 @@
+fruits = ["Apple", "Watermelon", "Lychee", "Orange", "Grapes", "Mango"]
+
+# print all data in list
+print("\n-----Whole List-------------------------")
+print(fruits)
+
+# print data on index 0
+print("\n-----Get 1 data [0]---------------------")
+print(fruits[0])
+
+# print data starting from the last
+print("\n-----From the last use negative [-2]----")
+print(fruits[-2])
+
+print("\n-----Edit-------------------------------")
+fruits[3] = "Strawberry"
+print(fruits)
+
+print("\n-----Add/Append-------------------------")
+fruits.append("Kiwi")
+print(fruits)
+
+print("\n-----Add Multiple/Extend----------------")
+fruits.extend(["Banana", "Raspberry", "Durian"])
+print(fruits)
+
+
+print("-------------")
