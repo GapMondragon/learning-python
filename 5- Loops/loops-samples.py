420c62fbc385ebe4441cddb91886488c91e85059
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-11 07:25:22 +0800

    range in loop

--- 5- Loops/loops-samples.py
+++ 5- Loops/loops-samples.py
@@ -1,10 +1,27 @@
 fruits = ["Apple", "Peach", "Pear"]
 print(fruits)
+
 print("-----")
 
 # foreach fruit in list(fruits)
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+
+print("-----")
+
+
+# range
+# range(start,stop,step)
+for number in range(1, 10, 3):
+    print(number)
+
+print("-----")
+
+# add 1+2+3+4...100
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
 
 print("-----")
