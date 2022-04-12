f020e2dfc3acb5f1329b39413afd87ac226e698a
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-12 20:21:13 +0800

    Reeborg

--- 6 - Functions and While Loops/functions-samples.py
+++ 6 - Functions and While Loops/functions-samples.py
@@ -1,7 +1,35 @@
+print("\n-----Function-----\n")
 # define function
 def my_function():
     print("Hello")
     print("Bye")
-    
+
 # call function
 my_function()
+
+print("\n-----While Loop-----\n")
+# keep on executing while loop 
+# as long as condition is not met
+#   while not at_goal():
+
+
+# practice here:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+#   def turn_right():
+    # for step in range(3):
+    #     turn_left()
+
+    # def jump():
+    #     turn_left()
+    #     move()
+    #     turn_right()
+    #     move()
+    #     turn_right()
+    #     move()
+    #     turn_left()
+        
+    # while not at_goal():
+    #     if wall_in_front():
+    #         jump()
+    #     else:
+    #         move()
