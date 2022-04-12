84e30a0bf9cd44fbcc63171fbe0c0fec8c86fe3c
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-12 21:16:20 +0800

    Maze

--- 6 - Functions and While Loops/functions-samples.py
+++ 6 - Functions and While Loops/functions-samples.py
@@ -36,3 +36,25 @@ print("\n-----While Loop-----\n")
 #         jump()
 #     else:
 #         move()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# # touch wall
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# # navigate touching right side
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
