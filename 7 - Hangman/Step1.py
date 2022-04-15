08d2c8e1574e22a7bd26c8f166d6c49aa1b409a5
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-15 21:59:56 +0800

    step 1

---
+++ 7 - Hangman/Step1.py
@@ -0,0 +1,27 @@
+#Step 1 
+
+word_list = ["dog", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+import random
+
+# Choose random word from list
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+# ask for a letter then convert to lowercase
+guess = input("Guess a letter: ").lower()
+
+# No need to convert string to list
+# it will convert the chosen string into a list when using for
+for letter in chosen_word:
+    if guess == letter:
+        print("Right")
+    else:
+        print("Wrong")
+
