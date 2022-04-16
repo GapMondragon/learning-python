9bbabde05d9159f785133d247abc313e8693f078
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-16 16:47:08 +0800

    step 2 of hangman done

--- 7 - Hangman/Step2.py
+++ 7 - Hangman/Step2.py
@@ -2,9 +2,30 @@
 
 word_list = ["dog", "baboon", "camel"]
 
+
+import random
+
+# Choose random word from list
+chosen_word = random.choice(word_list)
+print(f"{chosen_word}\n")
+
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+
+display = []
+for eachletter in chosen_word:
+    display += "_"
+print(display)
+
+
+joined = " ".join(display)
+print(joined)
+
+# ask for a letter then convert to lowercase
+guess = input("Guess a letter: ").lower()
+print("\n")
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
@@ -13,27 +34,13 @@ word_list = ["dog", "baboon", "camel"]
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-import random
+word_length = len(chosen_word)
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
 
-# Choose random word from list
-chosen_word = random.choice(word_list)
-print(f"{chosen_word}\n")
 
-display = []
-for eachletter in chosen_word:
-    display.append("_")
 print(display)
-
-# ask for a letter then convert to lowercase
-guess = input("Guess a letter: ").lower()
-print("\n")
-
-# No need to convert string to list
-# it will convert the chosen string into a list when using for
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
 
 print("\n")
