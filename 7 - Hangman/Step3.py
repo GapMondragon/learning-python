828d46b79f6e90b0c3848f5114cf9a0a18b35095
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-04-18 17:58:11 +0800

    end_of_game

---
+++ 7 - Hangman/Step3.py
@@ -0,0 +1,53 @@
+#Step 1 
+
+word_list = ["dog", "baboon", "camel"]
+
+
+import random
+
+# Choose random word from list
+chosen_word = random.choice(word_list)
+print(f"{chosen_word}\n")
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+
+display = []
+for eachletter in chosen_word:
+    display += "_"
+print(display)
+
+
+joined = " ".join(display)
+print(joined)
+
+end_of_game = False
+while not end_of_game:
+    # ask for a letter then convert to lowercase
+    guess = input("Guess a letter: ").lower()
+    print("\n")
+
+    #TODO-2: - Loop through each position in the chosen_word;
+    #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+    #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+    #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+    #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+    word_length = len(chosen_word)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+        # we still have to edit this to track the lives
+    print("\n")
+
+print("\n")
