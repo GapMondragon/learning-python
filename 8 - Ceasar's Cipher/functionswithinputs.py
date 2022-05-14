5bcbac8098b5b0c710e1f7d72ddf2a87a518e550
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-15 05:08:24 +0800

    functions with inputs and parameters

---
+++ 8 - Ceasar's Cipher/functionswithinputs.py
@@ -0,0 +1,25 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+print("----------------")
+
+def greet():
+    print("Good morning!")
+    print("Have you had your coffee?")
+    print("Have a good day ahead!")
+
+greet()
+
+print("----------------")
+
+def greetWithName(name):
+    print(f"Good morning {name}!")
+    print(f"Have you had your coffee {name}?")
+    print("Have a good day ahead!")
+
+greetWithName("Gap")
+
+print("----------------")
