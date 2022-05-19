f48eaac1829fcd6d551a593c19fc31a332173403
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-19 16:06:52 +0800

    Secret Auction

--- 9 - Dictionaries, Nesting, Secret Auction/SecretAuction.py
+++ 9 - Dictionaries, Nesting, Secret Auction/SecretAuction.py
@@ -1,19 +1,43 @@
+
+# Visualize your code here
+# https://pythontutor.com/visualize.html#mode=display
+
+from ArtSecretAuction import logo
+print(logo)
+
 def clear() -> None:
     """Clear the terminal."""
     print("\033[H\033[2J", end="", flush=True)
 
-# Printing Some Text
-print(1)
-print(2)
-print(3)
-print(4)
-print(5)
-print("Screen will now be cleared in 5 Seconds")
-clear()
-print(6)
-print(7)
-print(8)
-print(9)
-print(10)
-# Waiting for 5 seconds to clear the screen
+bids = {}
+more_bidders = True
 
+while more_bidders == True:
+   print("Welcome to the secret auction program.")
+   name = input("What is your name? ")
+   bid = input("What is your bid? $")
+
+   bids[name] = int(bid) 
+
+   other_bidders = input("Are there any other bidders? Type 'yes' or 'no' ")
+   if other_bidders == "no":
+      more_bidders = False
+   clear()
+
+
+highestbidder = ""
+highestbid = 0
+
+for bidder in bids:
+   # print(bidder)
+   # print(bids[bidder])
+   if bids[bidder] > highestbid:
+      highestbidder = bidder
+      highestbid = bids[bidder]
+
+
+
+print("Secret Auction Over.")
+print(f"The highest bidder is: {highestbidder} with a bid of ${highestbid}")
+print("Congratulations!!!")
+# print(bids)
