558b03f05797367f4070ba9e0448d32607ccac05
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-20 08:30:20 +0800

    Return - Functions with output

---
+++ 10 - Functions with Output - Return/Return - Format name.py
@@ -0,0 +1,20 @@
+# functions with output
+# using Return
+# putting a returned output into a variable
+# output = myfunction()
+
+
+f_name = input("What is your first name? ")
+l_name = input("What is your Last name? ")
+
+# turn gabriel paolo -> Gabriel Paolo
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    # print(f"{formatted_l_name}, {formatted_f_name}")
+    return f"{formatted_l_name}, {formatted_f_name}"
+
+
+# format_name(f_name, l_name)
+output = format_name(f_name, l_name)
+print(output)
