187a19c48bebd05bf463012ef852057b7b17383b
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-21 07:23:14 +0800

    Leap year v2

---
+++ 10 - Functions with Output - Return/Leap Year v2.py
@@ -0,0 +1,46 @@
+# Days in Months v2
+# copied from Day 3 leap year exercise
+
+
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year,month):
+    if month < 1 or month > 12:
+        return "Invalid Month"
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+    is_leapyear = is_leap(year)
+    if is_leapyear == True:
+        month_days[1] = 29
+        print("This is a leap year.")
+    chosen_month = month_days[month-1]
+    return f"That has {chosen_month} days."
+
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
