e34afc790c1a9798ae1c7a813ec08f28e56e1f37
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-21 09:20:06 +0800

    comments for docstring

--- 10 - Functions with Output - Return/Docstring - description for functions.py
+++ 10 - Functions with Output - Return/Docstring - description for functions.py
@@ -1,7 +1,7 @@
 # docstring
 # Take a first and last name and format it to
 #  return the title case version of the name
-
+# docstring must be in the first line afterhe function declaration
 
 f_name = input("What is your first name? ")
 l_name = input("What is your Last name? ")
@@ -9,6 +9,7 @@ l_name = input("What is your Last name? ")
 def format_name(f_name, l_name):
     '''Take a first and last name and format it to
      return the title case version of the name'''
+    # docstring must be in the first line
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     return f"{formatted_l_name}, {formatted_f_name}"
