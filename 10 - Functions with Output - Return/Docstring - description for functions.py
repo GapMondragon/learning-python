38b32daa129eda7e629018808acd80f6d680a9ba
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-21 09:14:07 +0800

    Docstring

---
+++ 10 - Functions with Output - Return/Docstring - description for functions.py
@@ -0,0 +1,19 @@
+# docstring
+# Take a first and last name and format it to
+#  return the title case version of the name
+
+
+f_name = input("What is your first name? ")
+l_name = input("What is your Last name? ")
+
+def format_name(f_name, l_name):
+    '''Take a first and last name and format it to
+     return the title case version of the name'''
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_l_name}, {formatted_f_name}"
+
+
+# hover the format name in line 18
+output = format_name(f_name, l_name)
+print(output)
