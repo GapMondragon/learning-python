5f722605d873cd3329e4aa281415b045937b279f
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-21 11:04:34 +0800

    Calculator with Recursion

--- 10 - Functions with Output - Return/Calculator.py
+++ 10 - Functions with Output - Return/Calculator.py
@@ -1,3 +1,5 @@
+from artcalculator import logo
+
 # add
 def add(n1,n2):
     return n1+n2
@@ -18,17 +20,31 @@ operations = {
     "/" : divide
 }
 
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
+# be careful of infinite loops/recursions 
+# recursion (putting everything inside so that you could call it again)
+def calculator():
+    print(logo)
+    num1 = float(input("What is the first number? "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-operation_symbol = input("Pick an operation: ")
+    while should_continue:
+        
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the second number? "))
 
-# e.g. * .... calculation_function = multiply
-calculation_function = operations[operation_symbol]
+        # e.g. * .... calculation_function = multiply
+        calculation_function = operations[operation_symbol]
 
-answer = calculation_function(num1,num2)
+        answer = calculation_function(num1,num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation. \n: ") == ("y"):
+            num1 = answer
+        else:
+            should_continue = False
+            # recursion
+            calculator()
+
+calculator()
