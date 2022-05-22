03f19ffcd473af1e657c80d0336a44a1fc506a5d
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-22 18:45:11 +0800

    notes for Calculator

--- 10 - Functions with Output - Return/Calculator.py
+++ 10 - Functions with Output - Return/Calculator.py
@@ -24,22 +24,31 @@ operations = {
 # recursion (putting everything inside so that you could call it again)
 def calculator():
     print(logo)
+
+    # get first number
     num1 = float(input("What is the first number? "))
 
+    # display symbols from dictionary as choices
     for symbol in operations:
         print(symbol)
-    should_continue = True
 
+    # while loop for continuous calculations
+    should_continue = True
     while should_continue:
         
+        # choose operation
         operation_symbol = input("Pick an operation: ")
+
+        # choose 2nd number
         num2 = float(input("What is the second number? "))
 
-        # e.g. * .... calculation_function = multiply
+        # assign the value from the dictionary depending on which key the user selected
         calculation_function = operations[operation_symbol]
 
+        # use selected value as a function
         answer = calculation_function(num1,num2)
 
+        # choose to proceed with while loop, to continue or not
         if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation. \n: ") == ("y"):
             num1 = answer
         else:
