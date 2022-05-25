5e65706149daf82e923c0c0bee1ad6d5c1428854
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-26 05:38:41 +0800

    adding more cards to user

--- 11 - BlackJack/BlackJack.py
+++ 11 - BlackJack/BlackJack.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 import random
 playerCards = []
 computerCards = []
@@ -10,13 +11,59 @@ for x in range(2):
 
 playerSum = sum(playerCards)
 computerSum = sum(computerCards)
-print(f"Player has: {playerCards} \n Total: {playerSum}")
-print(f"Computer has: {computerCards} \n Total: {computerSum}")
+comHiddenCard = []
 
-if computerSum == 21:
-    print("Computer has BlackJack. Computer Wins!")
-if playerSum == 21:
+comHiddenCard = computerCards
+comHiddenCard[1] = "X"
+
+def playerPhaseShowCards():
+    playerSum = sum(playerCards)
+    print("\n-----------------\n")
+    print(f"Player has: {playerCards}")
+    print(f"Total: {playerSum}")
+    print(f"Computer has: {computerCards}")
+    # print(f"Total: {computerSum}")
+    print("-----------------\n")
+
+
+def initialDrawVerdict():
     if computerSum == 21:
-        print("Draw!")
-    else:
-        print("Player Wins!")
+        print("Computer has BlackJack. Computer Wins!")
+    if playerSum == 21:
+        if computerSum == 21:
+            print("Draw!")
+        else:
+            print("Player Wins!")
+
+
+
+def aceCheck(playercomputer):
+    currentCards = (playercomputer)
+    replaceAceContinue = True
+    while replaceAceContinue:
+        i = 0
+        if currentCards[i] == 11:
+            currentCards[i] = 1
+            replaceAceContinue = False
+        i += 1
+    print(currentCards)
+
+def drawCardPlayerPhase():
+    anotherCard = input("Do you want another card? y or n \n")
+    if anotherCard == "y":
+        playerCards.append(random.choice(cards))
+        playerSum = sum(playerCards)
+        playerPhaseShowCards()
+        if playerSum > 21:
+            print("Over 21! You Lose. :p")
+        else:
+            drawCardPlayerPhase()
+
+playerPhaseShowCards()
+initialDrawVerdict()
+drawCardPlayerPhase()
+
+
+
+
+
