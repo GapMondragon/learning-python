b588fc8487ec079477a53fb683074a0003226921
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-05-25 17:05:42 +0800

    cards list give 2 cards for comp and player total, next will be comparing

---
+++ 11 - BlackJack/BlackJack.py
@@ -0,0 +1,14 @@
+import random
+playerCards = []
+computerCards = []
+
+cards = [11,1,2,3,4,5,6,7,8,9,10,10,10,10]
+
+for x in range(2):
+    playerCards.append(random.choice(cards))
+    computerCards.append(random.choice(cards))
+
+playerSum = sum(playerCards)
+computerSum = sum(computerCards)
+print(f"Player has: {playerCards} \n Total: {playerSum}")
+print(f"Computer has: {computerCards} \n Total: {computerSum}")
